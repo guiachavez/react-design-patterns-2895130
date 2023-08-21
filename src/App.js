b41d42a9b4b80e980063050ed708de8b1d538d0e
guiachavezpd <guia.chavez@peakdesign.com>
2023-08-21 16:26:14 -0700

    data source

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
+import axios from 'axios';
 import { CurrentUserLoader } from './CurrentUserLoader';
 import { UserLoader } from './UserLoader';
 import { ResourceLoader } from './ResourceLoader';
 import { ProductInfo } from './ProductInfo';
 import { UserInfo } from './UserInfo';
+import { DataSource } from './DataSource';
+
+const getServerData = url => async () => {
+	const response = await axios.get(url)
+	return response.data
+}
+
+const getLocalStorageData = key => () => {
+	return localStorage.getItem(key)
+}
+
+const Text = ({message}) => {
+	return (<h1>{message}</h1>)
+ }
 
 function App() {
 	return (
 		<>
-		<ResourceLoader resourceUrl="/users/123" resourceName="user">
+		<DataSource getDataFunc={getServerData('/users/123')} resourceName='user'>
 			<UserInfo />
-		</ResourceLoader>
-		<ResourceLoader resourceUrl="/products/1234" resourceName="product">
-			<ProductInfo />
-		</ResourceLoader>
+		</DataSource>
+
+		<DataSource getDataFunc={getLocalStorageData('message')} resourceName='message'>
+			<Text />
+		</DataSource>
 		</>
 	);
 }
